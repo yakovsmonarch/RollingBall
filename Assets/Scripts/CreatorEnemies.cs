42996f7bd063b99f898b04030eb8ba095abcd7b2
yakovsmonarch <medvejonok07@gmail.com>
2021-09-11 15:12:49 +0800

    I made an enemy generator.

--- Assets/Scripts/CreatorEnemies.cs
+++ Assets/Scripts/CreatorEnemies.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class CreatorEnemies : MonoBehaviour
 {
+    [SerializeField] private GameObject _template;
+    [SerializeField] private Transform _playerTransform;
 
+    private float _waitingTime = 4;
+    private float _currentTime = 0;
+
+    private void Update()
+    {
+        _currentTime += Time.deltaTime;
+        if(_currentTime >= _waitingTime)
+        {
+            CrateEnemy();
+
+            _waitingTime = Random.Range(3, 5);
+            _currentTime = 0;
+        }
+    }
+
+    private void CrateEnemy()
+    {
+        Instantiate(_template, new Vector3(_playerTransform.position.x + 10, _playerTransform.position.y, 0), Quaternion.identity);
+    }
 }
