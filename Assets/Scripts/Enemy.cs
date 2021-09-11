0874524e0382f5e1ed0adbe70f610f92e0032ee4
yakovsmonarch <medvejonok07@gmail.com>
2021-09-11 18:14:51 +0800

    The death of the player.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Player player))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
 }
